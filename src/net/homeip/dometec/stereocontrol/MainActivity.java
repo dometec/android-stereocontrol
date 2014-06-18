8a2146047725a75b5cf43836a2e543c88e300417
Domenico Briganti <dometec@gmail.com>
2014-06-18 18:35:32 +0200

    aggiunte push notif

--- src/net/homeip/dometec/stereocontrol/MainActivity.java
+++ src/net/homeip/dometec/stereocontrol/MainActivity.java
@@ -1,10 +1,15 @@
 package net.homeip.dometec.stereocontrol;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,15 +18,50 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
 public class MainActivity extends Activity implements OnClickListener {
 
 	private TextView tvMessage;
 
+	private final static String SENDER_ID = "614770691258";
+
+	private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+	private static final String KEY_PROPERTY_REG_ID = "registration_id";
+	private static final String KEY_PROPERTY_APP_VERSION = "appVersion";
+	private static final String LOG_TAG = "MainActivity";
+
+	private GoogleCloudMessaging gcm = null;
+	private String regid = null;
+	private Context context = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fullscreen);
 		tvMessage = (TextView) findViewById(R.id.textMessage);
+
+		context = getApplicationContext();
+
+		if (checkPlayServices()) {
+
+			gcm = GoogleCloudMessaging.getInstance(this);
+			regid = getRegistrationId(context);
+
+			if (regid.isEmpty()) {
+				registerInBackground();
+			} else {
+				Log.d(LOG_TAG, "No valid Google Play Services APK found.");
+			}
+
+		} else {
+			Log.d(LOG_TAG, "No Google Play Services APK found.");
+		}
+
 	}
 
 	@SuppressWarnings("unchecked")
@@ -33,35 +73,35 @@ public class MainActivity extends Activity implements OnClickListener {
 		String command = "";
 
 		if (R.id.buttonFm == btn.getId())
-			command = "s32113";
+			command = "Sony,2113,9";
 
 		if (R.id.buttonOff == btn.getId())
-			command = "s53905";
+			command = "Sony,3905,9";
 
 		if (R.id.buttonPc == btn.getId())
-			command = "s32401";
+			command = "Sony,1377,9";
 
 		if (R.id.buttonVu == btn.getId())
-			command = "s51153";
+			command = "Sony,1153,10";
 
 		if (R.id.buttonVd == btn.getId())
-			command = "s53201";
+			command = "Sony,3201,10";
 
 		if (R.id.buttonCu == btn.getId())
-			command = "s3150";
+			command = "Sony,150,3";
 
 		if (R.id.buttonCd == btn.getId())
-			command = "s32198";
+			command = "Sony,2198,3";
 
 		if (R.id.buttonBass == btn.getId())
-			command = "s322793";
+			command = "Sony,22793,3";
 
 		if (R.id.buttonMute == btn.getId())
-			command = "s3641";
+			command = "Sony,641,1";
 
 		command += '\n';
 
-		new SendPacket(command, "192.168.0.177", 20118).execute();
+		new SendPacket(command, "192.168.0.14", 20118).execute();
 
 	}
 
@@ -101,6 +141,93 @@ public class MainActivity extends Activity implements OnClickListener {
 			return null;
 		}
 
+	}
+
+	private boolean checkPlayServices() {
+
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+		if (resultCode != ConnectionResult.SUCCESS) {
+			if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+				GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+			} else {
+				Log.d(LOG_TAG, "This device is not supported - Google Play Services.");
+				finish();
+			}
+			return false;
+		}
+
+		return true;
+	}
+
+	private String getRegistrationId(Context context) {
+
+		final SharedPreferences prefs = getGCMPreferences(context);
+
+		String registrationId = prefs.getString(KEY_PROPERTY_REG_ID, "");
+		if (registrationId.isEmpty()) {
+			Log.d(LOG_TAG, "Registration ID not found.");
+			return "";
+		}
+
+		int registeredVersion = prefs.getInt(KEY_PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+		int currentVersion = getAppVersion(context);
+		if (registeredVersion != currentVersion) {
+			Log.d(LOG_TAG, "App version changed.");
+			return "";
+		}
+
+		return registrationId;
+
+	}
+
+	private SharedPreferences getGCMPreferences(Context context) {
+		return getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+	}
+
+	private static int getAppVersion(Context context) {
+		try {
+			PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+			return packageInfo.versionCode;
+		} catch (NameNotFoundException e) {
+			throw new RuntimeException("Could not get package name: " + e);
+		}
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void registerInBackground() {
+
+		new AsyncTask() {
+
+			@Override
+			protected Object doInBackground(Object... params) {
+
+				String msg = "";
+
+				try {
+
+					if (gcm == null)
+						gcm = GoogleCloudMessaging.getInstance(context);
+
+					regid = gcm.register(SENDER_ID);
+
+					Log.d(LOG_TAG, "########################################");
+					Log.d(LOG_TAG, "Current Device's Registration ID is: " + regid);
+
+				} catch (IOException ex) {
+					ex.printStackTrace();
+					msg = "Error :" + ex.getMessage();
+					Log.d(LOG_TAG, "Error: " + msg);
+				}
+
+				return null;
+			}
+
+			protected void onPostExecute(Object result) {
+				// to do here
+			}
+
+		}.execute(null, null, null);
 	}
 
 }
